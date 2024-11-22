154a052bd51a3cc80715bb1c04c4052cb9598784
Atreya Tadepalli <atreya.tadepalli@duke.edu>
2024-11-22 00:50:08 -0500

    test without pandas

--- test_main.py
+++ test_main.py
@@ -17,16 +17,18 @@ spark = SparkSession.builder.master("local[*]").appName("test_app").getOrCreate(
 def test_extract_load():
 
     output_path = extract_load(url="https://shorturl.at/5YexG", 
-            file_path="data/heart-failure.csv")
+    file_path="data/heart-failure.csv",
+    directory="data")
     assert os.path.exists(output_path)
 
 def test_transform():
-    df = transform_table("heart-failure-load","heart-failure-transform")
+    df = transform_table("heart_failure_load","heart-failure-transform")
     assert df is None, "Dataframe is None; load_data failed."
 
 def test_query():
     extract_load(url="https://shorturl.at/5YexG", 
-            file_path="data/heart-failure.csv")
+    file_path="data/heart-failure.csv",
+    directory="data")
     transform_table("heart-failure-load","heart-failure-transform")
     assert query("SELECT AVG(age) FROM heart-failure-transform WHERE smoking=1") is None
     
