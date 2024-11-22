070ddae05b94bd14f7cf48de6178f7de00aad5b1
Atreya Tadepalli <atreya.tadepalli@duke.edu>
2024-11-22 01:36:57 -0500

    test

--- mylib/extract.py
+++ mylib/extract.py
@@ -1,18 +1,11 @@
 from pyspark.sql import SparkSession 
-from pyspark.sql.types import (
-    StructType, 
-    StructField, 
-    IntegerType, 
-    FloatType 
-)
 import os
 import requests
-
+import pandas as pd
 
 
 def extract_load(url="https://shorturl.at/5YexG", 
-    file_path="data/heart_failure.csv",
-    directory="data"):
+    file_path="data/heart_failure.csv"):
 
     if os.path.dirname(file_path):
         os.makedirs(os.path.dirname(file_path), exist_ok=True)
@@ -27,26 +20,10 @@ def extract_load(url="https://shorturl.at/5YexG",
     .config("spark.sql.catalog.spark_catalog",\
              "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
     .getOrCreate()
-    #hf_df=pd.read_csv(url)
-    #print(hf_df.head())
-    #heart_failure_df=spark.createDataFrame(hf_df)
-    schema = StructType([
-                StructField("age", IntegerType(), True),
-                StructField("anaemia", IntegerType(), True),
-                StructField("creatinine_phosphokinase", IntegerType(), True),
-                StructField("diabetes", IntegerType(), True),
-                StructField("ejection_fraction", IntegerType(), True),
-                StructField("high_blood_pressure", IntegerType(), True),
-                StructField("platelets", IntegerType(), True),
-                StructField("serum_creatinine", FloatType(), True),
-                StructField("serum_sodium", IntegerType(), True),
-                StructField("sex", IntegerType(), True),
-                StructField("smoking", IntegerType(), True),
-                StructField("time", IntegerType(), True),
-                StructField("DEATH_EVENT", IntegerType(), True)
-            ])
-    
-    heart_failure_df = spark.read.option("header", "true").schema(schema).csv(file_path)
+    hf_df=pd.read_csv(url)
+    print(hf_df.head())
+    heart_failure_df=spark.createDataFrame(hf_df)
+    print(type(heart_failure_df))
     heart_failure_df.write.format("delta").mode("append").saveAsTable("heart_failure_load")
     print("Successfully extracted and loads data")
     return file_path
