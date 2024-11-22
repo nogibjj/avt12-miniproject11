3eefa80645600d4a80e4c28b3f01cb10c77b864a
Atreya Tadepalli <atreya.tadepalli@duke.edu>
2024-11-22 00:02:11 -0500

    test updates

--- test_main.py
+++ test_main.py
@@ -6,7 +6,7 @@ Test goes here
 from pyspark.sql import SparkSession
 from mylib.extract import extract
 import os
-from mylib.transform_load import trans_load
+from mylib.transform_load import transform_table
 from mylib.query import query
 
 spark = SparkSession.builder.master("local[*]").appName("test_app").getOrCreate()
@@ -21,13 +21,9 @@ def test_extract_csv():
 
     assert os.path.exists(output_path)
 
-def test_load():
-    df = trans_load(spark, path = 'data/heart-failure.csv')
-    assert df is not None, "Dataframe is None; load_data failed."
-    assert df.count() > 0, "Dataframe is empty; load_data did not load data correctly."
+def test_transform():
+    df = transform_table("heart-failure-load","heart-failure-transform")
+    assert df is None, "Dataframe is None; load_data failed."
 
 def test_query():
-    result_table = trans_load(spark)
-    result_table.createOrReplaceTempView("test_query")
-    test_query12="SELECT AVG(age) FROM test_query WHERE smoking=1"
-    assert query(spark,result_table,test_query12) is None
+    assert query("SELECT AVG(age) FROM test_query WHERE smoking=1") is None
